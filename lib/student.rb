fa3d9091ece95e9ae30960a5591337df00a7c5ee
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-11 03:55:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,12 @@ class Student
   def initialize(name, grade, id = nil)
     @name, @grade, @id = name, grade, id
   end
+  
+  def self.create_table
+  end
+  
+  def self.drop_table
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
