540d9fbd9b2fd49a4ccb7a6a06deb3824ce96404
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-11 04:03:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,14 @@ class Student
   end
   
   def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def self.drop_table
