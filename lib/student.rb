3c2d86c0791131d1167285598f8ee848519a9517
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-11 03:41:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_reader :id
   
   def initialize(name, grade, id = nil)
-    @name, @grade = name, grade
+    @name, @grade, @id = name, grade, id
   end
 
   # Remember, you can access your database connection anywhere in this class
